b317ff7970e3225fcf74e9f51e5726a6af2864dd
thad <92632994+telepathiqq@users.noreply.github.com>
2022-04-20 15:53:53 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCelsius = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblKelvin = New System.Windows.Forms.Label()
+        Me.lblFahrenheit = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Temperature (in Celsius)"
+        '
+        'txtCelsius
+        '
+        Me.txtCelsius.Location = New System.Drawing.Point(153, 10)
+        Me.txtCelsius.Name = "txtCelsius"
+        Me.txtCelsius.Size = New System.Drawing.Size(100, 23)
+        Me.txtCelsius.TabIndex = 1
+        Me.txtCelsius.Text = " "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.SplitContainer1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 75)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(240, 125)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Results"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(3, 19)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label7)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label6)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lblKelvin)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lblFahrenheit)
+        Me.SplitContainer1.Size = New System.Drawing.Size(234, 103)
+        Me.SplitContainer1.SplitterDistance = 78
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Fahrenheit"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 15)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Kelvin"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(31, 60)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(18, 15)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "°F"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(31, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(19, 15)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "°K"
+        '
+        'lblKelvin
+        '
+        Me.lblKelvin.AutoSize = True
+        Me.lblKelvin.Location = New System.Drawing.Point(3, 16)
+        Me.lblKelvin.Name = "lblKelvin"
+        Me.lblKelvin.Size = New System.Drawing.Size(22, 15)
+        Me.lblKelvin.TabIndex = 3
+        Me.lblKelvin.Text = "---"
+        '
+        'lblFahrenheit
+        '
+        Me.lblFahrenheit.AutoSize = True
+        Me.lblFahrenheit.Location = New System.Drawing.Point(3, 60)
+        Me.lblFahrenheit.Name = "lblFahrenheit"
+        Me.lblFahrenheit.Size = New System.Drawing.Size(22, 15)
+        Me.lblFahrenheit.TabIndex = 2
+        Me.lblFahrenheit.Text = "---"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(13, 44)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(240, 23)
+        Me.btnConvert.TabIndex = 3
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(270, 212)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtCelsius)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "TempConv v.1.0"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCelsius As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblKelvin As Label
+    Friend WithEvents lblFahrenheit As Label
+    Friend WithEvents btnConvert As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim myInput As Integer
+        myInput = txtCelsius.Text
+        lblKelvin.Text = myInput + 273.15
+        lblFahrenheit.Text = (myInput * 9 / 5) + 32
+    End Sub
+End Class
